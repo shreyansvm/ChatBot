6e69c38256ba99a6d36b145c8014eaf5900b4369
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-16 23:49:54 -0700

    renamed my bot as - Coco. Developed a way to keep it in an loop for a maximum of 60 seconds or user wants to exit

--- myBot.py
+++ myBot.py
@@ -5,8 +5,8 @@ import random
 import logging
 import os
 
-import sys
-print(sys.executable)
+import time
+
 
 os.environ['NLTK_DATA'] = os.getcwd() + '/nltk_data'
 
@@ -16,31 +16,85 @@ from flask import Flask
 # from config import FILTER_WORDS
 import config
 
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "howdy", "how are you", "how")
-GREETING_RESPONSES = ["'sup bro", "hey", "Hola Amigo!"]
+# TODO : Assign user an avatar and call it by that name
 
-def byteResponds(message):
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "howdy", "how are you", "how")
+GREETING_RESPONSES = ["'sup bro", "hey", "Hola Amigo!", "howdy", "watcha doing?"]
+GREETING_RECEIVED = 0
+GREETING_EXCHANGED = 0
+### cocoBot initiates bye
+COCO_INIT_BYE = ["Hey buddy, gotta go.", "Sorry to interrupt, but I need to go", "my abuela calling, gotta go!"]
+# user says something
+COCO_FINAL_BYE = ["Ok bye!", "cya", "astalavista!", "bye", "you bet!"]
+
+### user initiates bye
+USER_INIT_BYE = ["Ok bye!", "cya", "astalavista!", "bye"]
+# cocoBot says something
+USER_INIT_BYE_COCO_RESP = ["Coolio, take care.", "bye", "astalavista!", "Cya", "you bet!"]
+
+def cocoResponds(message):
+    global GREETING_RECEIVED
+    global GREETING_EXCHANGED
     msg = TextBlob(message)
     for word in msg.words:
-        if word.lower() in GREETING_KEYWORDS:
-            #return random.choice(GREETING_RESPONSES)
-            print(random.choice(GREETING_RESPONSES))
+        if GREETING_RECEIVED == 0:
+            if word.lower() in GREETING_KEYWORDS:
+                GREETING_RECEIVED = 1
+                #return random.choice(GREETING_RESPONSES)
+                print("Coco >> ", random.choice(GREETING_RESPONSES))
+                GREETING_EXCHANGED = 1
 
+def cocoWantsABreak(type):
+    if type == "cocoInitBye":
+        print("Coco >> ", random.choice(COCO_INIT_BYE))
+    elif type == "cocoFinalBye":
+        print("Coco >> ", random.choice(COCO_FINAL_BYE))
+    elif type == "userInitCocoFinalBye":
+        print("Coco >> ", random.choice(USER_INIT_BYE_COCO_RESP))
 
-def byteBot(message):
-    print(message)
-    byteResponds(message)
+def cocoBot(message):
+    cocoResponds(message)
 
 # checking if we are running in the main scope
 if __name__ == "__main__":
     import sys
+    initialGreetings = 0
+
 
     # Take the input string passed while starting the program
-    # python myBot.py "Hi Byte"
+    # python myBot.py "Hi Coco"
     if (len(sys.argv) > 1):
         welcome = sys.argv[1]
     else:
-        welcome = "How are you, Byte?"
-    # TODO : how can you keep the bot alive for infinite duration OR until the user enters some keyword like 'Bye'
-    byteBot(welcome)
+        welcome = "How are you, Coco?"
 
+    runTime = 60 ;# seconds
+    startTime = time.time()
+
+    # TODO : how can you keep the bot alive for infinite duration OR until the user enters some keyword like 'Bye'
+    while 1:
+        if initialGreetings == 0:
+            cocoBot(welcome)
+        else:
+            cocoBot(response)
+        initialGreetings = 1
+
+        response = raw_input()
+        responseMsg = TextBlob(response)
+
+        itsTimeForBye = 0
+        for word in responseMsg.words:
+            if word.lower() in USER_INIT_BYE:
+                itsTimeForBye = 1
+
+        elapsed = time.time() - startTime
+
+        if elapsed >= runTime :
+            cocoWantsABreak("cocoInitBye")
+            response = raw_input()
+            cocoWantsABreak("cocoFinalBye")
+            break
+
+        if itsTimeForBye == 1:
+            cocoWantsABreak("userInitCocoFinalBye")
+            break
