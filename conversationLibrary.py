9dcff9c59da479dacb0515f4f544580d553cb649
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-17 22:56:33 -0700

    library of all types of contexts

---
+++ conversationLibrary.py
@@ -0,0 +1,19 @@
+# TODO : Assign user an avatar and call it by that name
+AVATAR = ["WHALE", "EARTHWORM", "ELEPHANT"]
+# Default user avatar
+user_avatar = ""
+AVATAR_USER_RESPONSE = ["nah", "no", "no way!", "Bye", "that's rude"]
+
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "howdy", "how are you", "how")
+GREETING_RESPONSES = ["'sup bro", "hey", "Hola Amigo!", "howdy", "watcha doing?"]
+GREETING_RECEIVED = 0
+GREETING_EXCHANGED = 0
+### cocoBot initiates bye
+COCO_INIT_BYE = ["Hey buddy, gotta go.", "Sorry to interrupt, but I need to go", "my abuela calling, gotta go!"]
+# user says something
+COCO_FINAL_BYE = ["Ok bye!", "cya", "astalavista!", "bye", "you bet!"]
+
+### user initiates bye
+USER_INIT_BYE = ["Ok bye!", "cya", "astalavista!", "bye"]
+# cocoBot says something
+USER_INIT_BYE_COCO_RESP = ["Coolio, take care.", "bye", "astalavista!", "Cya", "you bet!"]
