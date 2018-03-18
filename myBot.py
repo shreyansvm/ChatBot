9578609f844d790350b3c27d27c30f91a666f80c
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-17 22:57:56 -0700

    moved all functions to botFunctions.py and global variables to conversationLibrary.py

--- myBot.py
+++ myBot.py
@@ -1,87 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf- 8 -*-
 from __future__ import print_function, unicode_literals
-import random
 import logging
-import os
-
-import time
-
-
+import os , time , random
 os.environ['NLTK_DATA'] = os.getcwd() + '/nltk_data'
-
 from textblob import TextBlob
 from flask import Flask
-# # not working
-# from config import FILTER_WORDS
 import config
-
-# TODO : Assign user an avatar and call it by that name
-AVATAR = ["WHALE", "EARTHWORM", "ELEPHANT"]
-# Default user avatar
-user_avatar = ""
-AVATAR_USER_RESPONSE = ["nah", "no", "no way!", "Bye", "that's rude"]
-
-GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up", "howdy", "how are you", "how")
-GREETING_RESPONSES = ["'sup bro", "hey", "Hola Amigo!", "howdy", "watcha doing?"]
-GREETING_RECEIVED = 0
-GREETING_EXCHANGED = 0
-### cocoBot initiates bye
-COCO_INIT_BYE = ["Hey buddy, gotta go.", "Sorry to interrupt, but I need to go", "my abuela calling, gotta go!"]
-# user says something
-COCO_FINAL_BYE = ["Ok bye!", "cya", "astalavista!", "bye", "you bet!"]
-
-### user initiates bye
-USER_INIT_BYE = ["Ok bye!", "cya", "astalavista!", "bye"]
-# cocoBot says something
-USER_INIT_BYE_COCO_RESP = ["Coolio, take care.", "bye", "astalavista!", "Cya", "you bet!"]
-
-def cocoResponds(message):
-    global GREETING_RECEIVED, GREETING_EXCHANGED
-    msg = TextBlob(message)
-    for word in msg.words:
-        if GREETING_RECEIVED == 0:
-            if word.lower() in GREETING_KEYWORDS:
-                GREETING_RECEIVED = 1
-                #return random.choice(GREETING_RESPONSES)
-                print("Coco >> ", random.choice(GREETING_RESPONSES))
-                GREETING_EXCHANGED = 1
-
-def findIfUserLikesTheAvatar(avatarUserResp):
-    userHappyForAvatar = 1
-    avatarUserRespMsg = TextBlob(avatarUserResp)
-    for word in avatarUserRespMsg.words:
-        if word.lower() in AVATAR_USER_RESPONSE:
-            return 0
-
-    return userHappyForAvatar
-
-'''
-CocoBot assigns a random avatar to the user
-'''
-def cocoAssignsAvatar():
-    global user_avatar
-    avatar = random.choice(AVATAR)
-    print("Coco >> I love ", avatar, ". Can I call you - ", avatar, "?")
-    avatarUserResp = raw_input(str(user_avatar) + " >> ")
-
-    while findIfUserLikesTheAvatar(avatarUserResp) == 0:
-        AVATAR.remove(avatar)
-        # TODO : How will you handle 'IndexError: list index out of range' when all avatars from AVATAR are popped
-        avatar = random.choice(AVATAR)
-        print("Coco >> How about ", avatar, "?")
-        avatarUserResp = raw_input(str(user_avatar) + " >> ")
-
-'''
-If User or CocoBot wishes to end the chat
-'''
-def cocoWantsABreak(type):
-    if type == "cocoInitBye":
-        print("Coco >> ", random.choice(COCO_INIT_BYE))
-    elif type == "cocoFinalBye":
-        print("Coco >> ", random.choice(COCO_FINAL_BYE))
-    elif type == "userInitCocoFinalBye":
-        print("Coco >> ", random.choice(USER_INIT_BYE_COCO_RESP))
+from botFunctions import *
+from conversationLibrary import *
 
 
 def cocoBot(message):
@@ -107,17 +34,16 @@ if __name__ == "__main__":
     while 1:
         if initialGreetings == 0:
             cocoBot(welcome)
-            # TODO : calling here also doesn't work.
-            # cocoAssignsAvatar()
         else:
             cocoBot(response)
-        initialGreetings = 1
 
         response = raw_input()
         responseMsg = TextBlob(response)
-        # TODO : calling here is not working
-        cocoAssignsAvatar()
+        if initialGreetings != 1:
+            cocoAssignsAvatar()
+            initialGreetings = 1
 
+        print("After cocoAssignsAvatar()")
         itsTimeForBye = 0
         for word in responseMsg.words:
             if word.lower() in USER_INIT_BYE:
@@ -134,3 +60,5 @@ if __name__ == "__main__":
         if itsTimeForBye == 1:
             cocoWantsABreak("userInitCocoFinalBye")
             break
+
+        # TODO : if User doesn't enter for x seconds/minute, Coco should say something
